2fd649b714e9bc3e21b0698936d18d9a5c7311b6
Maroun Eid <marouneid130@gmail.com>
2026-06-14 14:49:53 +0300

    Add Task C: Pseudorange and Doppler Measurements

--- StarlinkSim.m
+++ StarlinkSim.m
@@ -11,29 +11,37 @@ writelines(subsetLines, "SUBSET.tle");
 
 sat = satellite(sc, "SUBSET.tle");
 
-[position, velocity, time] = states(sat, "CoordinateFrame", "ecef");
+[satPos, satVel, time] = states(sat, "CoordinateFrame", "ecef");
 
 viewer = satelliteScenarioViewer(sc);
 show(sat);
 groundTrack(sat);
 
 figure;
-plot(time, squeeze(position(1,:,1)));
-hold on;
-plot(time, squeeze(position(2,:,1)));
-plot(time, squeeze(position(3,:,1)));
-legend('x','y','z');
-title('Satellite 1 Position');
-exportgraphics(gcf, 'results/taskA/position_sat1.png', 'Resolution', 300);
 
-figure;
-plot(time, squeeze(velocity(1,:,1)));
+subplot(2,1,1);
+plot(time, squeeze(satPos(1,:,1))/1000, 'LineWidth', 1.2);
 hold on;
-plot(time, squeeze(velocity(2,:,1)));
-plot(time, squeeze(velocity(3,:,1)));
+plot(time, squeeze(satPos(2,:,1))/1000, 'LineWidth', 1.2);
+plot(time, squeeze(satPos(3,:,1))/1000, 'LineWidth', 1.2);
+grid on;
+title('Satellite 1 Position (ECEF)');
+xlabel('Time');
+ylabel('Position (km)');
+legend('x','y','z');
+
+subplot(2,1,2);
+plot(time, squeeze(satVel(1,:,1))/1000, 'LineWidth', 1.2);
+hold on;
+plot(time, squeeze(satVel(2,:,1))/1000, 'LineWidth', 1.2);
+plot(time, squeeze(satVel(3,:,1))/1000, 'LineWidth', 1.2);
+grid on;
+title('Satellite 1 Velocity (ECEF)');
+xlabel('Time');
+ylabel('Velocity (km/s)');
 legend('vx','vy','vz');
-title('Satellite 1 Velocity');
-exportgraphics(gcf, 'results/taskA/velocity_sat1.png', 'Resolution', 300);
+
+exportgraphics(gcf, 'results/taskA/sat1_state.png', 'Resolution', 300);
 
 
 %% Task B: Receiver and visible satellites
@@ -56,5 +64,66 @@ ac.LineWidth = 2;
 intervals = accessIntervals(ac);
 disp(intervals);
 writetable(intervals, 'results/taskB/satellite_intervals.csv');
+
+%% Task C: Generate pseudorange and Doppler measurements
+
+numSat = numel(sat);
+numTime = numel(time);
+
+rxECEF = lla2ecef([rxLat, rxLon, rxAlt]);
+rxPos = repmat(rxECEF', 1, numTime);
+rxVel = zeros(3, numTime);
+
+range = NaN(numTime, numSat);
+rangeRate = NaN(numTime, numSat);
+pseudorange = NaN(numTime, numSat);
+doppler = NaN(numTime, numSat);
+
+c = physconst("LightSpeed");
+fc = 11.325e9;   % Example Starlink Ku-band frequency
+
+for k = 1:numSat
+    for i = 1:numTime
+
+        r_vec = satPos(:, i, k) - rxPos(:, i);
+        v_vec = satVel(:, i, k) - rxVel(:, i);
+
+        r_norm = norm(r_vec); 
+        range(i, k) = r_norm;
+
+        r_rate = dot(r_vec, v_vec) / r_norm;
+        rangeRate(i, k) = r_rate;
+
+        pseudorange(i, k) = r_norm;  % Noiseless
+        doppler(i, k) = -r_rate * fc / c;
+    end
+end
+
+k = 1;
+
+figure;
+
+subplot(3,1,1);
+plot(time, range(:,k)/1000, 'LineWidth', 1.5);
+title("Range");
+ylabel("km");
+grid on;
+
+subplot(3,1,2);
+plot(time, pseudorange(:,k)/1000, 'LineWidth', 1.5);
+title("Pseudorange");
+ylabel("km");
+grid on;
+
+subplot(3,1,3);
+plot(time, doppler(:,k), 'LineWidth', 1.5);
+title("Doppler");
+ylabel("Hz");
+xlabel("Time");
+grid on;
+exportgraphics(gcf, 'results/taskC/measurements_sat1.png', 'Resolution', 300);
+
+
+
 
 
